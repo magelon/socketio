b63330703a145b4d10991c0520f78dc1d3d06127
lon <alfardproject@gmail.com>
2016-02-27 15:27:13 -0800

    emit on
    emit(,)
    .on(,function())

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var io = require('socket.io').listen(4000);
 
-io.socket.on('connection',function(socket){
+io.sockets.on('connection',function(socket){
 	socket.on('my event',function(content){
 		console.log(content);
 	});
